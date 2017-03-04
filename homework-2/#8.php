e8c167bcab1e1a00571e79166c547cb39e52f2ba
Oleg Unger <ounger@inbox.ru>
2017-03-04 20:56:48 +0300

    -

--- homework-2/#8.php
+++ homework-2/#8.php
@@ -1,10 +1,19 @@
 <?php
 function smile()
-{
-    echo 'smile';
+{   echo '<pre>';
+    echo "                                                     ..::''''::..".'<br>';
+    echo "                                           .:::.   .;''        ``;.".'<br>';
+    echo "   ....                                    :::::  ::    ::  ::    ::".'<br>';
+    echo " ,;' .;:                ()  ..:            `:::' ::     ::  ::     ::".'<br>';
+    echo " ::.      ..:,:;.,:;.    .   ::   .::::.    `:'  :: .:' ::  :: `:. ::".'<br>';
+    echo "  '''::,   ::  ::  ::  `::   ::  ;:   .::    :   ::  :          :  ::".'<br>';
+    echo ",:';  ::;  ::  ::  ::   ::   ::  ::,::''.    .    :: `:.      .:' ::".'<br>';
+    echo "`:,,,,;;' ,;; ,;;, ;;, ,;;, ,;;, `:,,,,:'   :;:    `;..``::::''..;'".'<br>';
+    echo "                                                     ``::,,,,::''".'<br>';
+    echo '</pre>';
 }
 
-$text = 'Rx packets:950381 errors:0 dropped:0 overruns:0 frame:0 .';
+$text = 'Rx packets:950381 errors:0 dropped:0 overruns:0 frame:0 :).';
 $regex = '/(?<=packets:)[0-9]*/';
 $count = implode(preg_match($regex, $text, $arr), $arr);
 
