b0ffe631b56a4bbc23cccd1110a58d402c980336
Oleg Unger <ounger@inbox.ru>
2017-03-05 22:18:12 +0300

    правки сделаны

--- homework-2/#9.php
+++ homework-2/#9.php
@@ -1,3 +1,11 @@
 <?php
+function read_file($file)
+{
+    if (file_exists($file)) {
+        echo file_get_contents($file);
+    } else {
+        echo "Такого файла не существует";
+    }
+}
 
-echo file_get_contents('test.txt');
+read_file('test.txt');
