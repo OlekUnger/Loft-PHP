c0a36f50fc3b029003c27e5425017aa5d70bf491
Oleg Unger <ounger@inbox.ru>
2017-03-05 20:10:25 +0300

    -

--- homework-2/10.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = fopen('anothertest.txt','w');
-$str = 'Hello, again!';
-fwrite($file, $str);
-fclose($file);
-
-echo file_get_contents('anothertest.txt');
--- homework-2/6.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('Europe/Moscow');
-echo date('d.m.Y H:i');
-echo '<br>';
-//вариант 1:
-echo date('d.m.Y H:i:s', 1456261200);
-echo '<br>';
-//вариант 2:
-echo date('d.m.Y H:i:s', mktime(0,0,0,2,24,2016));
